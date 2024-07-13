0f8e415aae59be1b7e389e052989b028954c70d8
CharlesBLive <charbiffin+github@gmail.com>
2024-07-12 22:07:59 -0700

    Player Halo
    Added a player banner named halo that will show the players name and a color that the player selects. this banner may also include a health bar once it is complete.

--- runtimes/Unity/Assets/PlayerPresenceController.cs
+++ runtimes/Unity/Assets/PlayerPresenceController.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Xml;
 using UnityEngine;
 using UnityEngine.UIElements;
 
@@ -7,10 +8,14 @@ namespace Theta
 {
     public class PlayerPresenceController : MonoBehaviour
     {
-        public string name;
-        public Color color;
+        public string playerName;
+        public Color bannerColor;
         public VisualTreeAsset mainUI;
         public StyleSheet[] seasonalThemes;
+
+        private UIDocument uiDocument;
+        private VisualElement haloContainer;
+        private Label nameLabel;
 
 
         //make a flag/bubble over the players head that shows their
@@ -19,13 +24,29 @@ namespace Theta
         // Start is called before the first frame update
         void Start()
         {
-        
-        }
+            // Ensure UI Document component is assigned and correctly set up
+            uiDocument = GetComponentInChildren<UIDocument>();
+            if (uiDocument != null)
+            {
+                var root = uiDocument.rootVisualElement;
+                haloContainer = root.Q<VisualElement>("HaloContainer");
+                nameLabel = root.Q<Label>("NameDisplay");
 
-        // Update is called once per frame
-        void Update()
-        {
-        
+                if (haloContainer != null && nameLabel != null)
+                {
+                    // Set the player's name and banner color
+                    nameLabel.text = playerName;
+                    haloContainer.style.backgroundColor = new StyleColor(bannerColor);
+                }
+                else
+                {
+                    Debug.LogError("UI Elements not found in the UXML.");
+                }
+            }
+            else
+            {
+                Debug.LogError("UIDocument component not found on the player or its children.");
+            }
         }
     }
 }
